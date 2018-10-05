c5ffed09ce54a030c82e53dd590a169a775a8ad6
joshua <mama27j@gmail.com>
2018-10-06 00:09:24 +0300

    refresh

--- forum/src/main/java/com/fahamu/tech/chat/forum/PayActivity.java
+++ forum/src/main/java/com/fahamu/tech/chat/forum/PayActivity.java
@@ -1,5 +1,8 @@
 package com.fahamu.tech.chat.forum;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,13 +10,30 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PayActivity extends AppCompatActivity {
+    /**
+     * parameters
+     * 1.email
+     * 2.number
+     * 3.dsc
+     * 4.amount
+     * 5.callback
+     */
+    private static String PAY = "https://us-central1-money-fast-firebase.cloudfunctions.net/send_money?";
+
+    private FirebaseAuth firebaseAuth;
 
     @BindView(R.id.pay_swipe)
     SwipeRefreshLayout swipeRefreshLayout;
@@ -37,19 +57,51 @@ public class PayActivity extends AppCompatActivity {
             supportActionBar.setTitle("Pay");
         }
 
-        fab.setOnClickListener(view ->
-                Snackbar.make(view,
-                        "Selected amount : " + getIntent().getStringExtra("_amount"),
-                        Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show());
+        //auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null) {
+            startActivity(new Intent(this, SignUpActivity.class));
+            finish();
+        } else {
+            //swipe layout
+            initUI();
+
+            fab.setOnClickListener(view ->
+                    Snackbar.make(view,
+                            "Selected amount : " + getIntent().getStringExtra("_amount"),
+                            Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show());
+        }
     }
 
-    private void swipeInit(){
-        swipeRefreshLayout.setRefreshing(true);
-
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initUI() {
+        String params = "email=" + firebaseAuth.getCurrentUser().getEmail() +
+                "&dsc=Payment For Kemifra App&amount=1000&callback=doctor-fahamutech.firebaseapp.com";
+        webView.loadUrl(PAY + params);
+        webView.setWebViewClient(new MyWebClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            webView.loadUrl(PAY + params);
+        });
     }
 
-    private void pay(){
+    private class MyWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            swipeRefreshLayout.setRefreshing(true);
+        }
 
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            swipeRefreshLayout.setRefreshing(false);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                                    String description, String failingUrl) {
+            Snackbar.make(view, "Error loading page", Snackbar.LENGTH_LONG).show();
+        }
     }
 }
